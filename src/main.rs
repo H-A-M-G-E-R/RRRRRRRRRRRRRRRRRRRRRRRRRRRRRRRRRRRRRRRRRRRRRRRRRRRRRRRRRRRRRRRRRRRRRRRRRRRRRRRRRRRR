7b02872eb35376ebadbe587fbe7b266092db3e01
H-A-M-G-E-R <98665992+H-A-M-G-E-R@users.noreply.github.com>
2022-05-29 16:11:43 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -104,7 +104,7 @@ fn setup(
 
     // Mesh material.
     let mesh_material = materials.add(StandardMaterial {
-        base_color: Color::rgb_u8(rng.gen_range(0..=255), rng.gen_range(0..=255), rng.gen_range(0..=255)),
+        base_color: Color::rgb_u8(thread_rng().gen_range(0..=255), thread_rng().gen_range(0..=255), thread_rng().gen_range(0..=255)),
         metallic: 1000000.0,
         ..Default::default()
     });

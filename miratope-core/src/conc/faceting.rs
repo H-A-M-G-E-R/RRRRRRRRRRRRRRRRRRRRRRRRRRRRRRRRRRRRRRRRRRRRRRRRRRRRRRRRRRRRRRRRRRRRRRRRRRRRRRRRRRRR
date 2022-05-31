388af766f64f7680efcda1cafaeaa41659f4feaa
H-A-M-G-E-R <98665992+H-A-M-G-E-R@users.noreply.github.com>
2022-05-31 16:57:41 -0500

    Seperate vertex orbits

--- miratope-core/src/conc/faceting.rs
+++ miratope-core/src/conc/faceting.rs
@@ -853,7 +853,8 @@ impl Concrete {
                             continue
                         }
                     }
-		    if seperate_vertex_orbits && &orbit_of_vertex[vertex] != orbit {
+		    let oov = orbit_of_vertex[vertex]
+		    if seperate_vertex_orbits && oov as usize != orbit {
 		        continue
 		    }
                     let mut new_orbit = Vec::new();

436ea059a9988ec4d769f0e9af28d501baba4f70
galoomba1 <69735183+galoomba1@users.noreply.github.com>
2022-07-02 22:35:43 +0200

    Update faceting.rs

--- miratope-core/src/conc/faceting.rs
+++ miratope-core/src/conc/faceting.rs
@@ -22,7 +22,7 @@ pub enum GroupEnum {
     Chiral(bool),
 }
 
-const CL: &str = "          \r                                                                               \r";
+const CL: &str = "\r                                                                                                                   \r";
 
 const DELAY: u128 = 200;
 
@@ -914,7 +914,7 @@ fn faceting_subdim(
                     }
                     if now.elapsed().as_millis() > DELAY && print_faceting_count {
                         print!("{}", CL);
-                        print!("{:.79}", format!("{} facets found, {} skipped, {:?}", output.len(), skipped, facets));
+                        print!("{:.115}", format!("{} facets found, {} skipped, {:?}", output.len(), skipped, facets));
                         std::io::stdout().flush().unwrap();
                         now = Instant::now();
                     }
@@ -924,7 +924,7 @@ fn faceting_subdim(
 
                     if now.elapsed().as_millis() > DELAY && print_faceting_count {
                         print!("{}", CL);
-                        print!("{:.79}", format!("{} facets found, {:?}", output.len(), facets));
+                        print!("{:.115}", format!("{} facets found, {:?}", output.len(), facets));
                         std::io::stdout().flush().unwrap();
                         now = Instant::now();
                     }
@@ -1557,7 +1557,7 @@ impl Concrete {
         'l: loop {
             if now.elapsed().as_millis() > DELAY {
                 print!("{}", CL);
-                print!("{:.79}", format!("{} facetings, {:?}", output_facets.len(), facets));
+                print!("{:.115}", format!("{} facetings, {:?}", output_facets.len(), facets));
                 std::io::stdout().flush().unwrap();
                 now = Instant::now();
             }

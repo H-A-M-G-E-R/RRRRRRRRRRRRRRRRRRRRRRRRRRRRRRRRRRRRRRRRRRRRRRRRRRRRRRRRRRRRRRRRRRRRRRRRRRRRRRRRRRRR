53bb62a2d3a91f6f198057fe4645637ef595a5b0
H-A-M-G-E-R <justindinhbui@gmail.com>
2022-07-10 20:59:31 -0500

    facet petrie-coxeter polytopes

--- miratope-core/src/conc/faceting.rs
+++ miratope-core/src/conc/faceting.rs
@@ -1026,6 +1026,7 @@ impl Concrete {
         min_hyperplane_copies: Option<usize>,
         max_hyperplane_copies: Option<usize>,
         max_hyperplane_orbits: Option<usize>,
+        skew_rank: Option<usize>,
         include_compounds: bool,
         include_compound_elements: bool,
         mark_fissary: bool,
@@ -1039,7 +1040,7 @@ impl Concrete {
         exotic_elements: bool,
 		uniform: bool
     ) -> Vec<(Concrete, Option<String>)> {
-        let rank = self.rank();
+        let rank = if let Some(sr) = skew_rank {sr+1} else {self.rank()};
         let mut now = Instant::now();
 
         if rank < 3 {
@@ -1209,7 +1210,7 @@ impl Concrete {
 
                     let hyperplane = Subspace::from_points(points.iter());
 
-                    if hyperplane.is_hyperplane() {
+                    if hyperplane.rank() == rank - 2 {
                         let inradius = hyperplane.distance(&Point::zeros(self.dim().unwrap()));
                         if let Some(min) = min_inradius {
                             if inradius < min - f64::EPS {
